3669933c8daeb0866328a3c539e89c503d235ca5
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-16 15:58:41 -0300

    feat: :sparkles: add project structure
    add project structure with node.js (express), typescript, typeorm, postgres.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import routes from "./routes";
+import 'reflect-metadata'
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3000, () =>
+  console.log("⚡ Server started at http://localhost:3000")
+);
