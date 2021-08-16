c2323718333e70749fae3cb930aa323a4f4da075
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-16 19:37:08 -0300

    feat: :sparkles: add encryption for password
    add encryption for password using bcryptjs

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,4 +1,5 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BeforeInsert, BeforeUpdate } from "typeorm";
+import bcrypt from 'bcryptjs'
 @Entity("users")
 class User {
   @PrimaryGeneratedColumn("uuid")
@@ -9,6 +10,13 @@ class User {
 
   @Column()
   password: string;
+  
+  @BeforeInsert()
+  @BeforeUpdate()
+  hashPassword(){
+    this.password = bcrypt.hashSync(this.password, 8)
+  }
+
 }
 
 export default User;
