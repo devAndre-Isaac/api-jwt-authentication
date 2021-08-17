95680f807f4439a5133915b99c62ef3e86466518
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-17 16:15:54 -0300

    feat: :sparkles: add controller for auth

---
+++ src/app/controllers/AuthController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import bcrypt from "bcryptjs";
+import User from "../models/User";
+import jwt from "jsonwebtoken";
+
+class AuthController {
+  async authenticate(req: Request, res: Response) {
+    const repository = getRepository(User);
+    const { email, password } = req.body;
+
+    const user = await repository.findOne({ where: { email } });
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.sendStatus(401);
+    }
+    const token = jwt.sign({ id: user.id }, "secret", { expiresIn: "1d" });
+
+    delete user.password;
+
+    return res.json({
+      user,
+      token,
+    });
+  }
+}
+//TODO PAREI NO 32:00
+export default new AuthController();
