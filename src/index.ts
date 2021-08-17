7b3bc95960fcd0bcda1732a0082b7f0dd448ffc0
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-17 16:14:54 -0300

    style: :art: change server return

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import routes from "./routes";
-import 'reflect-metadata'
-import './database/connect'
+import "reflect-metadata";
+import "./database/connect";
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
 app.listen(3000, () =>
-  console.log("⚡ Server started at http://localhost:3000")
+  console.log("%c⚡Server started at http://localhost:3000", "color:red")
 );
